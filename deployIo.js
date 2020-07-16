864eb6d098b19b578f937465b0da0852b9341c20
Austin Griffith <austin@ethereum.org>
2020-07-16 12:25:28 -0600

    cleanup

--- deployIo.js
+++ deployIo.js
@@ -1,72 +1,36 @@
-const { exec } = require('child_process')
-const fs = require("fs")
-const awsCreds = JSON.parse(fs.readFileSync("aws.json").toString().trim())
-const s3 = require('s3');
-const winston = require('winston')
-const AWS = require('aws-sdk')
-let site = "scaffoldeth.io"
-let cloudfrontDistro = "E37UIV99XUBM8T"
+const s3FolderUpload = require('s3-folder-upload')
+const fs = require('fs')
+const directoryName = 'public'
 
-const localDir = "public"
+const BUCKETNAME = "scaffoldeth.io"   // <<---- SET YOUR BUCKET NAME AND CREATE aws.json ** see below vvvvvvvvvv
 
-//Setup
-var client = s3.createClient({
-  s3Options: awsCreds,
-});
-uploadParams = buildS3Params(site)
-
-//Prep index
-//let index = fs.readFileSync("build/index.html").toString();
-//index = index.split("\"\/").join("\"");
-//winston.debug(index);
-//fs.writeFileSync("build/index.html",index);
-
-//Upload
-fs.readdir( localDir+"/" , function( err, files ) {
-  if( err ) {
-    console.error( "Could not list the directory.", err );
-    process.exit( 1 );
-  }
-
-  var uploader = client.uploadDir(uploadParams);
-  uploader.on('error', function(err) {
-    console.error("unable to sync:", err.stack);
-  });
-  uploader.on('progress', function() {
-    console.log("progress", uploader.progressAmount, uploader.progressTotal);
-  });
-  uploader.on('end', function() {
-    console.log("done uploading "+site);
-    var cloudfront = new AWS.CloudFront(new AWS.Config(awsCreds));
-    var cfparams = {
-      DistributionId: cloudfrontDistro,
-      InvalidationBatch: {
-        CallerReference: ''+(new Date()),
-        Paths: {
-          Quantity: 1,
-          Items: ["/*"]
-        }
-      }
-    };
-    cloudfront.createInvalidation(cfparams, function(err, data) {
-      if (err) reject(err, err.stack); // an error occurred
-      else     console.log(data);           // successful response
-    });
-  });
-})
-
-
-function buildS3Params(site) {
-  site = site.toLowerCase()
-
-  var uploadParams = {
-    localDir: localDir,
-    s3Params: {
-      Bucket: site,
-      Prefix: "",
-      ACL: "public-read"
-    }
-  }
-
-  return uploadParams
+if(!BUCKETNAME){
+  console.log('☢️   Enter a bucket name in packages/react-app/s3.js ')
+  process.exit(1)
 }
+
+let credentials = {}
+try{
+  credentials = JSON.parse(fs.readFileSync("aws.json"))
+}catch(e){
+  console.log(e)
+  console.log('☢️   Create an aws.json credentials file in packages/react-app/ like { "accessKeyId": "xxx", "secretAccessKey": "xxx", "region": "xxx" } ')
+  process.exit(1)
+}
+console.log("credentials",credentials)
+
+credentials.bucket = BUCKETNAME
+
+// optional options to be passed as parameter to the method
+const options = {
+  useFoldersForFileTypes: false,
+  useIAMRoleCredentials: false
+}
+
+// optional cloudfront invalidation rule
+const invalidation = {
+  awsDistributionId: "E37UIV99XUBM8T",
+  awsInvalidationPath: "/*"
+}
+
+s3FolderUpload(directoryName, credentials, options, invalidation)
